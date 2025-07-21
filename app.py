52710fcbe228c01a925c9f87d09cf018721a5a4b
ChristineKakina <christinekakina@gmail.com>
2025-07-21 17:14:07 +0300

    Updates: flask basic

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/about')
+def about():
+    return 'About Page'
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/hello')
+def hello():
+
+    return render_template("index.html", name ="Student")
+
+@app.route('/submit',methods=['POST'])
+def submit():
+    userText = request.form.get('text', 'No text submitted')
+    return f"You submitted {userText}"
+
+@app.route('/home')
+def home():
+    return "Hello Flask!"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
